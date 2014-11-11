5900efc9b838551004e439bc4bb3c45b1e37a28a
Vicky Qijing Zhang <vickyqijingzhang@gmail.com>
2014-11-11 13:53:48 -0600

    Update and rename game.java to Game.java
    final version as of 11/11/14

---
+++ Game.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Game {
+
+	private static Scanner in;
+
+	public static void main(String[] args)
+	{
+		Player playera = new Player(0);
+		Player playerb = new Player(1);
+				
+		in = new Scanner(System.in);
+		
+		System.out.println("Welcome to the 1111 game! You are Player A, and the computer is Player B. You both start with 1 in both hands.");
+		System.out.println("It's your turn now.");
+		
+		System.out.print("Player A: ");
+        playera.returnResult();
+                
+        System.out.print("Player B: ");
+        playerb.returnResult();
+		System.out.println("If you want to make the hit yourself, press h. ");
+		System.out.println("If you want the computer to make a more rational move for you, press r.");
+			
+		playera.HitProcess(playerb);
+			
+	}
+	
+}
